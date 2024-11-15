8b3f273812b4ce97062104efaf627ce2e1ef164b
Phco23 <142793213+Phco23@users.noreply.github.com>
2024-11-15 09:50:28 +0700

    Add project files.

---
+++ eLearning/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eLearning.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
